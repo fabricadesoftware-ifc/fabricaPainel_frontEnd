d4c73695be5352a646b5f2d31581c73acf90db3f
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-07-19 19:14:28 -0300

    FEAT: add 'field test' view #6

---
+++ src/views/tests/FieldTestView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { onMounted } from 'vue'
+
+import { useFieldStore } from '@/stores';
+
+const fieldStore = new useFieldStore()
+
+onMounted(() => {
+    fieldStore.getFields()
+})
+
+</script>
+
+<template>
+    <div>
+        <h1>Field testing</h1>
+        <h3>Actual number of fields: {{ fieldStore.fieldCount }}</h3>
+        <ul>
+            <li v-for="field in fieldStore.state.fields">
+                {{ field }}
+            </li>
+        </ul>
+    </div>
+</template>
