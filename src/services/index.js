b9cb3262958fa404156c54f586b786f9c8d56be8
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-07-19 19:11:57 -0300

    FEAT: add missing functions #6

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,7 @@
 import KeywordService from '../services/others/keyword';
+import FieldService from "@/services/field"
 
-export { KeywordService };
+export {
+    KeywordService,
+    FieldService,
+}
