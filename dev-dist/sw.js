4d1524968eec9a095479c40ea49881e36c084a26
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-07-24 22:33:16 -0300

    FIX: remove redundant file

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gok37qbh10g"
+    "revision": "0.ocja21hrjq8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,11 @@
-import axios from 'axios'
+import axios from "axios"
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/'
-}) 
+    baseURL: "http://localhost:8000/api/",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    timeout: 10000,
+})
 
 export default api
