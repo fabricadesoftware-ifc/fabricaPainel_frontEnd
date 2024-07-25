3f961b9fa33cac5e1e62c0745b34895b57ec4251
tecinfoifc <luansilva250807@gmail.com>
2024-07-25 11:28:06 -0300

    FEAT: words limit enabled in input #18

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.so5c6cp7nvg"
+    "revision": "0.6co7pthava8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/views/others/HomeView.vue
+++ src/views/others/HomeView.vue
@@ -1,8 +1,20 @@
-<script setup></script>
+<script setup>
+import { computed, ref } from "vue";
+  const text = ref('')
+  const limit = 255
+  const errmsg = computed(() =>{
+    if(text.value.length > limit){
+      return 'o campo deve ter no maximo 255 caracteres' 
+    }
+    return 'cadastre seu trabalho max: 255 caracteres'
+  })
+</script>
 <template>
   <div>
     <h1>Home</h1>
     <p>Home page content</p>
+    <input placeholder="texto" v-model="text">
+    <p>{{errmsg}} - {{text.length}}</p>
   </div>
 </template>
 <style scoped>
