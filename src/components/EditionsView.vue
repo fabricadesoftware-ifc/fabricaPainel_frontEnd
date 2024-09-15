5ecaa336113c55fde5289b7b9261a67ea3e52098
jonatasperaza <jonatassilvaperaza@gmail.com>
2024-09-14 21:31:50 -0300

    refactor: Atualizar componente EditionsView para exibir anos múltiplos no intervalo

--- src/components/EditionsView.vue
+++ src/components/EditionsView.vue
@@ -4,13 +4,9 @@
       <v-col cols="2">
         <v-sheet>
           <v-card variant="tonal">
-            <v-toolbar color="primary">
-              <v-toolbar-title>Filtros</v-toolbar-title>
-            </v-toolbar>
-
             <v-card-text>
               <h2 class="text-h6 mb-2">Categorias</h2>
-              <v-chip-group v-model="amenities" column multiple>
+              <v-chip-group v-model="amenitiesLocal" column multiple>
                 <v-chip
                   v-for="category in store.state.category"
                   :key="category.id"
@@ -62,23 +58,52 @@
 </template>
 
 <script lang="ts" setup>
-  import { ref } from 'vue'
   import { useRouter } from 'vue-router'
   import { usefilter } from '@/stores/editions'
 
+  interface Edition {
+    id: number
+    year: string
+    edition_name: string
+    text: string
+    to: string
+    status: boolean
+    category: Array<number>
+  }
+  const amenitiesLocal: number[] = []
   const store = usefilter()
   const router = useRouter()
-  const amenities = ref<string[]>([])
 
   const updateYearRange = (range: number[]) => {
     store.state.yearsRange = range
   }
 
+  watch(amenitiesLocal, amenities => {
+    updateAmenities(amenities)
+  })
+  const updateAmenities = (amenities: number[]) => {
+    store.state.amenities = amenities
+  }
+
   const filteredEditions = computed(() => {
     const [minYear, maxYear] = store.state.yearsRange
-    return store.state.editions.filter((edition: any) => {
+
+    // Filtra as edições pelo intervalo de anos
+    const editionsInYearRange = store.state.editions.filter((edition: any) => {
       const editionYear = parseInt(edition.year, 10)
       return editionYear >= minYear && editionYear <= maxYear
+    })
+
+    // Se nenhuma categoria estiver selecionada, exibe todas as edições no intervalo de anos
+    console.log(store.state)
+    if (store.state.amenities.length === 0) {
+      return editionsInYearRange
+    }
+
+    // Caso contrário, filtra pelas categorias selecionadas
+    return editionsInYearRange.filter((edition: Edition) => {
+      // Verifica se a edição tem alguma categoria que corresponde às selecionadas
+      return edition.category.some((category: number) => store.state.amenities.includes(category))
     })
   })
 
