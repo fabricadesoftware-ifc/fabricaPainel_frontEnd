ae28f9f5410900303dc2304e38d07a3f62769b43
Eduardo da Silva <eduardo.silva@gmail.com>
2024-10-17 14:11:02 -0300

    FEAT [WORKS]: Add two columns to work listing

--- src/components/PaperAcceptanceTable.vue
+++ src/components/PaperAcceptanceTable.vue
@@ -1,109 +1,118 @@
 <script setup>
-  import { ref } from 'vue'
-  import { Status } from '@/interfaces/work'
-  import { useWork } from '@/stores/work'
-  import jsPDF from 'jspdf'
-  import 'jspdf-autotable'
-  import { useAuth } from '@/stores/auth'
+import { ref } from "vue";
+import { Status } from "@/interfaces/work";
+import { useWork } from "@/stores/work";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+import { useAuth } from "@/stores/auth";
 
-  const authStore = useAuth()
-  const workStore = useWork()
-  const search = ref('')
-  const props = defineProps({
-    title: String,
-    works: Array,
-  })
+const authStore = useAuth();
+const workStore = useWork();
+const search = ref("");
+const props = defineProps({
+  title: String,
+  works: Array,
+});
 
-  const getColor = name => {
-    if (name === 0) return ''
-    if (name === 1) return 'grey'
-    if (name === 3) return 'blue'
-    return 'green'
+const getColor = (name) => {
+  if (name === 0) return "";
+  if (name === 1) return "grey";
+  if (name === 3) return "blue";
+  return "green";
+};
+
+const selectedStatus = ref([]);
+const selectedFields = ref([]);
+const selectedThemes = ref([]);
+
+const status = () => {
+  const statusString = [];
+  for (let i = 1; i <= 4; i++) {
+    statusString.push(Status[i].replace(/_/g, " "));
   }
+  return statusString;
+};
 
-  const selectedStatus = ref([])
-  const selectedFields = ref([])
-  const selectedThemes = ref([])
+const worksHeaders = [
+  { title: "Status", key: "status" },
+  { title: "Titulo do Trabalho", key: "title" },
+  { title: "Área", key: "field" },
+  { title: "Tema Transversal", key: "cross_cutting_theme" },
+  { title: "Data de Submissão", key: "initial_submission_work_date" },
+  { title: "Orientador", key: "advisor.name" },
+  { title: "Coorientador", key: "co_advisor.name" },
+];
 
-  const status = () => {
-    const statusString = []
-    for (let i = 1; i <= 4; i++) {
-      statusString.push(Status[i].replace(/_/g, ' '))
+const generatePDF = () => {
+  if (props.works.length === 0) {
+    alert("Não há trabalhos para gerar o PDF");
+    return;
+  }
+  // eslint-disable-next-line new-cap
+  const doc = new jsPDF();
+
+  doc.text(props.title, 14, 20);
+
+  const tableData = props.works.map((work) => [
+    Status[work.status].replace(/_/g, " "),
+    work.title,
+    work.field.map((field) => field.name).join(", "),
+    work.cross_cutting_theme.name,
+    workStore.coverteData(work.initial_submission_work_date),
+  ]);
+
+  doc.autoTable({
+    head: [worksHeaders.map((header) => header.title)],
+    body: tableData,
+    startY: 30,
+  });
+
+  doc.save(`${props.title}.pdf`);
+};
+const works = computed(() => {
+  return props.works.filter((work) => {
+    // return selectedStatus.value.includes(Status[work.status].replace(/_/g, ' '))
+    if (
+      selectedStatus.value.length > 0 &&
+      !selectedStatus.value.includes(Status[work.status].replace(/_/g, " "))
+    ) {
+      return false;
     }
-    return statusString
-  }
-
-  const worksHeaders = [
-    { title: 'Status', key: 'status' },
-    { title: 'Titulo do Trabalho', key: 'title' },
-    { title: 'Área', key: 'field' },
-    { title: 'Tema Transversal', key: 'cross_cutting_theme' },
-    { title: 'Data de Submissão', key: 'initial_submission_work_date' },
-  ]
-
-  const generatePDF = () => {
-    if (props.works.length === 0) {
-      alert('Não há trabalhos para gerar o PDF')
-      return
+    if (
+      selectedFields.value.length > 0 &&
+      !work.field.some((field) => selectedFields.value.includes(field.name))
+    ) {
+      return false;
     }
-    // eslint-disable-next-line new-cap
-    const doc = new jsPDF()
+    if (
+      selectedThemes.value.length > 0 &&
+      !selectedThemes.value.includes(work.cross_cutting_theme.name)
+    ) {
+      return false;
+    }
+    return true;
+  });
+});
 
-    doc.text(props.title, 14, 20)
+const fields = computed(() => {
+  const fields = [];
+  props.works.forEach((work) => {
+    work.field.forEach((field) => {
+      if (!fields.includes(field.name)) fields.push(field.name);
+    });
+  });
+  return fields;
+});
 
-    const tableData = props.works.map(work => [
-      Status[work.status].replace(/_/g, ' '),
-      work.title,
-      work.field.map(field => field.name).join(', '),
-      work.cross_cutting_theme.name,
-      workStore.coverteData(work.initial_submission_work_date),
-    ])
-
-    doc.autoTable({
-      head: [worksHeaders.map(header => header.title)],
-      body: tableData,
-      startY: 30,
-    })
-
-    doc.save(`${props.title}.pdf`)
-  }
-  const works = computed(() => {
-    return props.works.filter(work => {
-      // return selectedStatus.value.includes(Status[work.status].replace(/_/g, ' '))
-      if (
-        selectedStatus.value.length > 0 &&
-        !selectedStatus.value.includes(Status[work.status].replace(/_/g, ' '))
-      ) { return false }
-      if (
-        selectedFields.value.length > 0 &&
-        !work.field.some(field => selectedFields.value.includes(field.name))
-      ) { return false }
-      if (
-        selectedThemes.value.length > 0 &&
-        !selectedThemes.value.includes(work.cross_cutting_theme.name)
-      ) { return false }
-      return true
-    })
-  })
-
-  const fields = computed(() => {
-    const fields = []
-    props.works.forEach(work => {
-      work.field.forEach(field => {
-        if (!fields.includes(field.name)) fields.push(field.name)
-      })
-    })
-    return fields
-  })
-
-  const themes = computed(() => {
-    const themes = []
-    props.works.forEach(work => {
-      if (!themes.includes(work.cross_cutting_theme.name)) { themes.push(work.cross_cutting_theme.name) }
-    })
-    return themes
-  })
-
+const themes = computed(() => {
+  const themes = [];
+  props.works.forEach((work) => {
+    if (!themes.includes(work.cross_cutting_theme.name)) {
+      themes.push(work.cross_cutting_theme.name);
+    }
+  });
+  return themes;
+});
 </script>
 
 <template>
@@ -112,82 +121,29 @@
       <h1 class="text-h5 py-4 font-weight-bold">
         {{ props.title }}
       </h1>
-      <v-text-field
-        v-if="props.works.length > 1"
-        v-model="search"
-        class="pa-2"
-        clearable
-        hide-details
-        label="Pesquisar"
-        placeholder="Digite para pesquisar..."
-        prepend-inner-icon="mdi-magnify"
-        rounded="xl"
-        variant="plain"
-      />
+      <v-text-field v-if="props.works.length > 1" v-model="search" class="pa-2" clearable hide-details label="Pesquisar"
+        placeholder="Digite para pesquisar..." prepend-inner-icon="mdi-magnify" rounded="xl" variant="plain" />
       <div class="filters d-flex align-center" v-if="authStore.isLogged && authStore.user.user_type == 'TEACHER'">
-        <v-select
-          v-model="selectedStatus"
-          class="pa-2"
-          clearable
-          hide-details
-          :items="status()"
-          label="Status"
-          max-width="33%"
-          multiple
-          placeholder="Selecione o status"
-          rounded="xl"
-          variant="plain"
-        />
-        <v-select
-          v-model="selectedFields"
-          class="pa-2"
-          clearable
-          hide-details
-          :items="fields"
-          label="Área"
-          max-width="33%"
-          multiple
-          placeholder="Selecione a área"
-          rounded="xl"
-          variant="plain"
-        />
-        <v-select
-          v-model="selectedThemes"
-          class="pa-2"
-          clearable
-          hide-details
-          :items="themes"
-          label="Tema Transversal"
-          max-width="33%"
-          multiple
-          placeholder
-          rounded="xl"
-          variant="plain"
-        />
+        <v-select v-model="selectedStatus" class="pa-2" clearable hide-details :items="status()" label="Status"
+          max-width="33%" multiple placeholder="Selecione o status" rounded="xl" variant="plain" />
+        <v-select v-model="selectedFields" class="pa-2" clearable hide-details :items="fields" label="Área"
+          max-width="33%" multiple placeholder="Selecione a área" rounded="xl" variant="plain" />
+        <v-select v-model="selectedThemes" class="pa-2" clearable hide-details :items="themes" label="Tema Transversal"
+          max-width="33%" multiple placeholder rounded="xl" variant="plain" />
       </div>
 
       <div class="rounded-xl">
-        <v-data-table
-          class="pa-2"
-          :headers="worksHeaders"
-          :items="works"
-          :search="search"
-        >
+        <v-data-table class="pa-2" :headers="worksHeaders" :items="works" :search="search">
           <!-- Conteúdo da tabela -->
           <template #item.name="{ item }">
-            <v-btn
-              class="pa-0 hover"
-              color="primary"
-              :to="'/panel/paper/:id'.replace(':id', item.id)"
-              variant="text"
-            >
+            <v-btn class="pa-0 hover" color="primary" :to="'/panel/paper/:id'.replace(':id', item.id)" variant="text">
               {{ item.name }}
             </v-btn>
           </template>
           <template #item.status="{ item }">
             <div class="text-start">
               <v-chip :color="getColor(item.status)">
-                {{ Status[item.status].replace(/_/g, ' ') }}
+                {{ Status[item.status].replace(/_/g, " ") }}
               </v-chip>
             </div>
           </template>
@@ -201,24 +157,13 @@
             {{ item.cross_cutting_theme.name }}
           </template>
           <template #item.title="{ item }">
-            <v-btn
-              class="pa-0 hover"
-              color="primary"
-              :to="'/panel/paper/:id'.replace(':id', item.id)"
-              variant="text"
-            >
+            <v-btn class="pa-0 hover" color="primary" :to="'/panel/paper/:id'.replace(':id', item.id)" variant="text">
               {{ item.title }}
             </v-btn>
           </template>
         </v-data-table>
-        <v-btn
-          v-if="authStore.user.user_type === 'TEACHER'"
-          block
-          class="mt-4"
-          color="blue"
-          variant="outlined"
-          @click="generatePDF"
-        >
+        <v-btn v-if="authStore.user.user_type === 'TEACHER'" block class="mt-4" color="blue" variant="outlined"
+          @click="generatePDF">
           Baixar PDF
         </v-btn>
       </div>
--- src/services/works.ts
+++ src/services/works.ts
@@ -1,78 +1,86 @@
-import api from '@/plugins/api'
+import api from "@/plugins/api";
 
 class WorkService {
-  private handleError (error: any, action: string) {
-    console.error(`Error during ${action}:`, error)
-    throw new Error(`Failed to ${action} work`)
+  private handleError(error: any, action: string) {
+    console.error(`Error during ${action}:`, error);
+    throw new Error(`Failed to ${action} work`);
   }
 
-  async sendWork (work: any) {
+  async sendWork(work: any) {
     try {
-      const { data } = await api.post('/work/', work)
-      return data
+      const { data } = await api.post("/work/", work);
+      return data;
     } catch (error) {
-      this.handleError(error, 'create')
+      this.handleError(error, "create");
     }
   }
 
-  async getWork (id: string) {
+  async getWork(id: string) {
     try {
-      const { data } = await api.get(`/work/${id}/`)
-      return data
+      const { data } = await api.get(`/work/${id}/`);
+      return data;
     } catch (error) {
-      this.handleError(error, 'fetch')
+      this.handleError(error, "fetch");
     }
   }
 
-  async getWorks () {
+  async getWorks() {
     try {
-      const { data } = await api.get('/work/')
-      return data
+      const { data } = await api.get("/work/");
+      return data;
     } catch (error) {
-      this.handleError(error, 'fetch')
+      this.handleError(error, "fetch");
     }
   }
 
-  async getWorkByCrossCuttingTheme (crossCuttingTheme: string) {
+  async getWorkByCrossCuttingTheme(crossCuttingTheme: string) {
     try {
-      const { data } = await api.get(`/work/?cross_cutting_theme=${crossCuttingTheme}`)
-      return data
+      const { data } = await api.get(
+        `/work/?cross_cutting_theme=${crossCuttingTheme}`
+      );
+      return data;
     } catch (error) {
-      this.handleError(error, 'fetch')
+      this.handleError(error, "fetch");
     }
   }
 
-  async updateWork (workId: string, partialWorkData: any) {
+  async updateWork(workId: string, partialWorkData: any) {
     try {
-      const { data } = await api.patch(`/work/${workId}/`, partialWorkData)
-      return data
+      const { data } = await api.patch(`/work/${workId}/`, partialWorkData);
+      return data;
     } catch (error) {
-      this.handleError(error, 'patch')
+      this.handleError(error, "patch");
     }
   }
 
-  async getUserWorks (userType: string, userId: string) {
+  async getUserWorks(userType: string, userId: string) {
     try {
-      if (userType === 'STUDENT') {
-        const { data } = await api.get(`/work/?team_member_id=${userId}`)
-        return data
-      } else if (userType === 'TEACHER') {
-        const { data } = await api.get(`/work/?advisor_id=${userId}`)
-        return data
+      if (userType === "STUDENT") {
+        const { data } = await api.get(`/work/?team_member_id=${userId}`);
+        return data;
+      } else if (userType === "TEACHER") {
+        const works: any = [];
+        let response = await api.get(`/work/?advisor_id=${userId}`);
+        response.data.forEach((item: any) => works.push(item));
+        response = await api.get(`/work/?co_advisor_id=${userId}`);
+        response.data.forEach((item: any) => {
+          if (!works.find((work: any) => work.id === item.id)) works.push(item);
+        });
+        return works;
       }
     } catch (error) {
-      this.handleError(error, 'fetch')
+      this.handleError(error, "fetch");
     }
   }
 
-  async approveWork (verification_token: string) {
+  async approveWork(verification_token: string) {
     try {
-      const { data } = await api.get(`/accept-work/${verification_token}/`)
-      return data
+      const { data } = await api.get(`/accept-work/${verification_token}/`);
+      return data;
     } catch (error) {
-      this.handleError(error, 'approve')
+      this.handleError(error, "approve");
     }
   }
 }
 
-export default new WorkService()
+export default new WorkService();
