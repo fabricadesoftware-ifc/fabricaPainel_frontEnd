b9c1326cc14ec2c236ddb3040c3c77c054c81b13
jonatassilvaperaza@gmail.com <jonatassilvaperaza@gmail.com>
2024-10-18 11:33:35 -0300

    Ajustar responsividade do cabeçalho e adicionar subtexto no título H1

--- src/components/TitleH1.vue
+++ src/components/TitleH1.vue
@@ -1,18 +1,30 @@
 <template>
-  <h1 class="text-h1 text-start text-wrap H1" style="word-break: break-word" :class="color">
+  <h1 :class="[ color, 'text-start text-wrap ', screenWidth > 950 ? 'H1 text-h1' : 'text-h3 font-normal' ]" style="word-break: break-word">
     {{ props.text }}
+    <br>
+    {{ props.subtext }}
   </h1>
   <p class="pt-8">
     {{ content }}
   </p>
 </template>
 
-<script setup lang="ts">
+<script setup>
   const props = defineProps({
     text: String,
+    subtext: String,
     content: String,
     color: String,
   })
+const screenWidth = ref(0);
+
+onMounted(() => {
+  window.addEventListener("resize", () => {
+    screenWidth.value = window.innerWidth;
+  });
+  screenWidth.value = window.innerWidth;
+});
+
 </script>
 
 <style>
