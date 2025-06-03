c6e604819f1170e5aa7be6d5f3f1de9d45215c22
Gabz047 <gabe1612mail@gmail.com>
2025-06-03 13:42:11 -0300

    CHORE: Pastas das rotas organizadas

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,8 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ro9aak933"
-
+    "revision": "0.7i2ql1p9jho"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
---
+++ src/pages/panel/works/index.vue
@@ -0,0 +1,68 @@
+<script  setup>
+  import { useWork } from '@/stores/work';
+  import router from '@/router';
+  import { ref } from 'vue';
+  import CardSubmission from '@/components/steps/panel/CardSubmission.vue';
+  import { useEdition } from '@/stores/edition';
+  const workStore = useWork()
+  const EditionStore = useEdition()
+
+  const year = new Date().getFullYear()
+
+  const SubmissionVerify = computed(() => {
+    if(workStore.userWorks[0]?.edition?.year === year ){
+      return {
+        work: workStore.userWorks[0].edition.final_submission_date,
+        status: workStore.userWorks[0].status
+      }
+    }
+    return ''
+  })
+
+  const verifySubmitWork = computed(() => {
+    if(workStore.userWorks[0]?.edition?.year !== year && workStore.userWorks.length > 0){
+      return workStore.userWorks
+    }
+    else if (workStore.userWorks[0]?.edition?.year === year && workStore.userWorks.length > 0){
+      return workStore.userWorks.slice(1)
+    }
+    return []
+  })
+
+  const is_submit = computed(() => {
+      if(workStore.userWorks[0]?.edition?.year === year ){
+        return true
+      }
+      return false
+  })
+
+  onMounted( async () => {
+    await workStore.fetchUserWorks()
+    await EditionStore.fetchCurrentEdition()
+    console.log(workStore.userWorks[0])
+  })
+</script>
+<template>
+  <LayoutPanel>
+    <v-container class="w-100">
+      <div class="d-flex justify-space-between align-center text-h6">
+        <h1 class="font-weight-bold" style="font-size: 40px;">Submissões</h1>
+        <VChip :class="is_submit ? 'bg-green' : 'bg-red'">{{ is_submit ? 'trabalho submetido' : 'trabalho ainda não submetido' }}</VChip>
+      </div>
+      <div class="d-flex pa-10 flex-column w-100 justify-space-between align-center">
+        <CardSubmission :actual_title="EditionStore.currentEdition.theme" :work="SubmissionVerify?.work" :work_status="SubmissionVerify?.status"/>
+      </div>
+      <div class="d-flex justify-space-between align-center text-h6">
+        <h1 class="font-weight-bold" style="font-size: 30px;">Edições anteriores</h1>
+      </div>
+      <div >
+        <div v-if="verifySubmitWork.length > 0">
+          <CardSubmission v-for="works in verifySubmitWork" :key="works.id" :work="works.edition.final_submission_date" :work_status="2" :edition_title="works.edition.theme"/>
+        </div>
+        <div class="pa-5" v-else>
+            <h1 class="text-h6 text-center">Você não tem trabalhos submetidos em edições anteriores</h1>
+        </div>
+      </div>
+    </v-container>
+  </LayoutPanel>
+</template>
