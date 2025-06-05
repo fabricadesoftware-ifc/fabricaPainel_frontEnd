9e1dadd52d4fb7f2f2bd2a46044f8cfb894742c2
vinyzada <vinyescolas@gmail.com>
2025-06-04 23:04:17 -0300

    feat: criada página para visualização de edição individual #49

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.nr4iposl02g"
+    "revision": "0.2llnku1gv0o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
