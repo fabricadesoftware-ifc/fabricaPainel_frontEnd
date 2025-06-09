3fda0c1d57aabc11cf93c6110cfdd31f0d28b184
Gabz047 <gabe1612mail@gmail.com>
2025-06-09 12:11:44 -0300

    REFACTOR: Refatorada questao de listagem de propostas

--- src/pages/panel/works/index.vue
+++ src/pages/panel/works/index.vue
@@ -13,19 +13,28 @@ const UserStore = useAuth()
 const year = new Date().getFullYear()
 
 const SubmissionVerify = computed(() => {
-  const work = workStore.userWorks[0]
-  if (!work || !work.edition) {
-    return null;
+  
+  // const work = workStore.userWorks[0]
+  // if (!work || !work.edition) return null;
+
+  // if (work.edition.year === year) {
+  //   return {
+  //     work: work.edition.final_submission_date,
+  //     status: work.status,
+  //     id: work.id
+  //   }
+  // }
+  // return null;
+
+  const works = workStore.userWorks
+  if (!works) return null
+  else {
+    return works.filter((s)=> s.edition.year == year).sort((a, b) => {
+    if (a.status != 4) return -1
+    if (b.id === 4) return 1
+    return 0
+  })
   }
-    
-  if (work.edition.year === year) {
-    return {
-      work: work,
-      status: work.status,
-      id: work.id
-    }
-  }
-  return null;
 })
 
 const verifySubmitWork = computed(() => {
@@ -33,11 +42,12 @@ const verifySubmitWork = computed(() => {
   if (!Array.isArray(works) || works.length === 0) return []
   if (works.length === 0) return []
 
-  if (works[0]?.edition?.year !== year) {
-    return works
-  } else {
-    return works.slice(1)
-  }
+  // if (works[0]?.edition?.year !== year) {
+  //   return works
+  // } else {
+  //   return works.slice(1)
+  // }
+  return works.filter((s) => s.edition.year !== year )
 })
 
 const is_submit = computed(() => {
@@ -48,7 +58,6 @@ const is_submit = computed(() => {
 onMounted(async () => {
   await workStore.fetchUserWorks(UserStore.user.user_type, UserStore.user.id)
   await EditionStore.fetchCurrentEdition()
-
 })
 </script>
 
@@ -62,13 +71,17 @@ onMounted(async () => {
         </VChip>
       </div>
 
-      <div class="d-flex pa-10 flex-column w-100 justify-space-between align-center">
-        <CardSubmission
+      <div>
+        <div class="d-flex align-center mt-10 mb-10  ga-5 w-100">
+                <h1 class="text-h5 font-weight-bold" style="font-size: 30px;">{{ EditionStore.currentEdition.edition_name }}</h1>
+                        <VChip class="bg-blue d-flex justify-center align-center" pill style="width: 120px;">Em aberto
+                </VChip>
+        </div>
+        <CardSubmission v-for="(work, index) in SubmissionVerify" :key="index" 
           v-if="SubmissionVerify"
-          :actual_title="EditionStore.currentEdition.theme"
-          :work_id="SubmissionVerify.id"
-          :work="SubmissionVerify?.work"
-          :work_status="SubmissionVerify.status"
+          :work_id="work.id"
+          :work="work.edition.final_submission_date"
+          :work_status="work.status"
         />
          <CreateWork :date="new Date() < new Date(EditionStore?.currentEdition?.final_second_submission_date)" :edition_title="EditionStore?.currentEdition?.edition_name" v-else />
          
@@ -81,12 +94,11 @@ onMounted(async () => {
       <div>
         <div v-if="verifySubmitWork.length > 0">
           <CardSubmission
-            v-for="works in verifySubmitWork"
+            v-for="(works, index) in verifySubmitWork"
             :key="works.id"
             :work="works"
             :work_status="2"
-            :edition_title="works.edition.edition_name"
-            :work_id="works.id"
+            :edition_title="index == 0 ? works.edition.edition_name : ''"
           />
         </div>
         <div class="pa-5" v-else>
