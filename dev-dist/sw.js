259664b8b013ad31351ebb2a3f527761374324c2
mariaeduardanichelle <madunichelle@gmail.com>
2025-06-15 20:53:01 -0300

    FEAT: hide "edicao atual"  button on mobile screens

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3evla3t7oeg"
+    "revision": "0.1665gdueptg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
