2fefd549461d4edac00b3b01abffa76fcc16f084
Gabriel Alves <gabriel52.alves@gmail.com>
2025-06-18 11:28:33 -0300

    FEAT: refactor index page layout and improve responsiveness

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
 import { useScreen } from "@/composables/composables";
 import { useAuth } from "@/stores/auth";
-// import { useDisplay } from "vuetify";
+import { useDisplay } from "vuetify";
 
 const authStore = useAuth();
-
+const { lgAndUp } = useDisplay()
 const { isMobile } = useScreen();
 
 onMounted(() => {
@@ -23,25 +23,25 @@ function downloadPdf() {
 </script>
 
 <style>
-/* @media (max-width: 1250px) {
+@media (max-width: 1250px) {
   .custom-parallax {
     height: 1000px;
   }
-} */
+}
 </style>
 
 <template>
 
   <body>
     <v-parallax src="@/assets/home_banner.png" class="custom-parallax" gradient="rgba(0,0,0,0.69)">
-      <div class="my-8 mx-12">
+      <div class="my-8 mx-6">
         <header class="d-flex justify-space-between">
           <div class="d-flex align-center" style="user-select: none">
             <v-img src="@/assets/logotipo_painel_integracao.png" width="50" />
             <h2 class="font-weight-regular text-white ml-4">Painel</h2>
           </div>
           <div v-if="authStore.isLogged">
-            <v-btn class="font-weight-bold" rounded="pill" width="250px" height="60px" color="primary"
+            <v-btn class="font-weight-bold" rounded="pill" width="8vw" size="x-large" color="primary"
               to="/panel/works">Acessar Plataforma</v-btn>
           </div>
           <div v-else class="d-flex ga-4">
@@ -51,27 +51,28 @@ function downloadPdf() {
               to="/auth/login">Login</v-btn>
           </div>
         </header>
-        <main class="d-flex flex-column w-50 h-100">
-          <div class="h-screen d-flex flex-column justify-center">
-            <p class="d-flex text-white text-h1 font-weight-bold">
+        <div class="d-flex flex-column h-100" :class="lgAndUp ? 'w-50' : 'w-100'">
+          <div class="h-screen d-flex flex-column justify-center align-md-center align-lg-start">
+            <p class="d-flex text-white font-weight-bold"
+              :class="lgAndUp ? 'text-start text-h1' : 'text-center text-h4'">
               Painel da Semana de Integração
             </p>
-            <p class="mt-16 text-h5 text-white font-weight-regular">
+            <p class="mt-16 text-h5 text-white font-weight-light" :class="lgAndUp ? 'text-start' : 'text-center'">
               Painel designado a SEPE do IFC, que administra e avalia os
               trabalhos dos estudantes
             </p>
-            <div class="d-flex ga-5" style="margin-top: 150px">
-              <v-btn rounded="pill" color="primary" class="font-weight-regular" size="x-large" to="/panel/editions"
-                append-icon="mdi-arrow-right-circle">Edição atual
+            <div class="d-flex flex-wrap justify-lg-start justify-center mt-16 ga-5 ">
+              <v-btn :rounded="lgAndUp ? 'pill' : 'rounded-0'" color="primary" class="font-weight-regular"
+                size="x-large" to="/panel/editions" append-icon="mdi-arrow-right-circle">Edição atual
               </v-btn>
               <v-btn rounded="pill" size="x-large" class="font-weight-medium" append-icon="mdi-download"
-                @click="downloadPdf">
+                :variant="lgAndUp ? 'tonal' : 'text'" :color="lgAndUp ? 'white' : 'white'" @click="downloadPdf">
                 Orientações
               </v-btn>
             </div>
           </div>
-        </main>
-        <div class="d-flex mt-5" style="justify-content: end">
+        </div>
+        <div class="d-flex mt-5 justify-lg-end justify-center">
           <v-btn rounded="lg" variant="text" class="d-flex text-h5 align-center text-white" color="white"
             append-icon="mdi-arrow-down-bold-circle" style="">
             <v-text>Explore o painel</v-text>
