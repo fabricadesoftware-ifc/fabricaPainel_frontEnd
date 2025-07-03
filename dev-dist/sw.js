a8e5ab94a0b4ecffb6ad7746a3fed8d4b90fed75
Gabz047 <gabe1612mail@gmail.com>
2025-07-03 15:51:09 -0300

    FEAT: Arrumando

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0qfglvone1"
+    "revision": "0.rre3u7v8peo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
