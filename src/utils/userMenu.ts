beed323df953f18af80ad5e3a67d45ab2e8675d4
Gabz047 <gabe1612mail@gmail.com>
2025-07-03 13:41:09 -0300

    FEAT: Criado validação por tipo de usuário no botão de download

--- src/utils/userMenu.ts
+++ src/utils/userMenu.ts
@@ -1,7 +1,7 @@
 import { ref } from "vue";
       
 export const usersData = ref([  
-      { name: "Edição", link: "/panel/editions", action: 'navigate' },
+      { name: "Edições", link: "/panel/editions", action: 'navigate' },
       { name: "Propostas", link: "/panel/works", action: 'navigate' },
       { name: "Regulamentação", link: "", action: 'download' },
 ]);
