64d67cc6f16c308742bd6616ab7f198b9552a39b
Gabz047 <gabe1612mail@gmail.com>
2025-07-05 01:25:01 -0300

    FIX: Arrumado base url da api

--- src/plugins/api.ts
+++ src/plugins/api.ts
@@ -3,8 +3,7 @@ import { useAuth } from "@/stores/auth";
 import { globalRouter } from "./globalRouter";
 
 const api = axios.create({
-  // baseURL: 'https://painelapi.fabricadesoftware.ifc.edu.br/api',
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://painelapi.fabricadesoftware.ifc.edu.br/api',
   headers: {
     "Content-Type": "application/json",
   },
