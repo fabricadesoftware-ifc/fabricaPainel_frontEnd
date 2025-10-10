89f131638bf7796d2b1118cedf5d2bb11b8bbae1
Gabriel Alves <gabriel52.alves@gmail.com>
2025-10-10 10:24:41 -0300

    FIX: refactor conditional rendering for evaluator, advisor, and collaborator sections in TeacherContainer

--- src/components/work/members/TeacherContainer.vue
+++ src/components/work/members/TeacherContainer.vue
@@ -25,26 +25,28 @@ const checkWorks = () => {
 <template>
     <div class="w-100 d-flex justify-center flex-column ga-10">
         <div v-if="checkWorks()">
-        <div v-if="props.works.evaluatorWorks.length > 0 " class="w-100 d-flex align-center flex-column">
-        <h2>Avaliar</h2>
-        <slot name="evaluate">
+            <div v-if="(['TEACHER', 'TAE'].includes(props.user_type)) && (props.works.evaluatorWorks?.length > 0)"
+                class="w-100 d-flex align-center flex-column">
+                <h2>Avaliar</h2>
+                <slot name="evaluate">
 
-        </slot>
-        </div>
+                </slot>
+            </div>
 
-         <div v-if="props.user_type == 'TEACHER' && props.works.advisorWorks.length > 0" class="w-100 d-flex align-center flex-column mt-10">
-        <h2>Orientar</h2>
-        <slot name="advise">
+            <div v-if="props.user_type == 'TEACHER' && props.works.advisorWorks.length > 0"
+                class="w-100 d-flex align-center flex-column mt-10">
+                <h2>Orientar</h2>
+                <slot name="advise">
 
-        </slot>
-        </div>
+                </slot>
+            </div>
 
-         <div v-if="props.works.collaboratorWorks.length > 0 " class="w-100 d-flex align-center flex-column  mt-10">
-        <h2>Colaborar</h2>
-        <slot name="collaborate">
+            <div v-if="props.works.collaboratorWorks.length > 0" class="w-100 d-flex align-center flex-column  mt-10">
+                <h2>Colaborar</h2>
+                <slot name="collaborate">
 
-        </slot>
-        </div>
+                </slot>
+            </div>
         </div>
         <div v-else>
             <p>Você não foi chamado para participar de nenhum projeto.</p>
